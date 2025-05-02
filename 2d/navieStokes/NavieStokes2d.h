6b6de61060f82a40e954eb70f4700dcbc05bcf15
Kenji-inoue <65485292+Kenji-inoue@users.noreply.github.com>
2025-05-02 19:50:58 +0900

    Copied 2d burgers to prepare for 2d navie-stokes

---
+++ 2d/navieStokes/NavieStokes2d.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "typedef.h"
+#include "Advection2d.h"
+#include "Diffusion2d.h"
+
+class NavieStokes2d
+{
+public:
+    NavieStokes2d(int meshX, int meshY, double reynolds, 
+              double lx, double ly, double deltaT, Velocity2d f);
+    ~NavieStokes2d() = default;
+
+    Velocity2d calculate();
+    Value calculateTerm(const Field2d& f, const Velocity2d& velocity, int i, int j) const;
+    void validateTime();
+    void updateBoundaryCondition(Field2d& f);
+private:
+    void updateVelocity(Velocity2d& f);
+    const int MESH_X;
+    const int MESH_Y;
+    const double REYNOLDS;
+    const double DX;
+    const double DY;
+    const double DELTA_T;
+    Velocity2d m_f;
+    Diffusion2d diffusion_;
+    Advection2d advection_;
+};
---
+++ 2d/navieStokes/main.cpp
@@ -0,0 +1,47 @@
+#include "NavieStokes2d.h"
+#include "FieldUtil.h"
+#include <stdexcept>
+#include <iostream>
+
+void initializeVelocity(Velocity2d& f, int meshX, int meshY, double lx, double ly) {
+    const double PI=3.14159;
+    const double K = 2 * PI;
+    const double DX = lx / (meshX - 1);
+    const double DY = ly / (meshY - 1);
+    for (int j = 0; j < meshY; j++) {
+        for (int i = 0; i < meshX; i++) {
+            const auto x = i * DX;
+            const auto y = j * DY;
+            f.u[j][i] = -1 * cos(K * x) * sin(K * y);
+            f.v[j][i] = sin(K * x) * cos(K * y);
+        }
+    }
+}
+
+int main() {
+    int meshX = 10;
+    int meshY = meshX;
+    double reynolds = 200;
+    double lx = 1.0;
+    double ly = lx;
+    double deltaT = 0.02; 
+    Velocity2d f;
+    FieldUtil::setSize(f.u, meshX, meshY);
+    FieldUtil::setSize(f.v, meshX, meshY);
+    initializeVelocity(f, meshX, meshY, lx, ly);
+
+    try {
+        NavieStokes2d navieStokes(meshX, meshY, reynolds, lx, ly, deltaT, f);
+        const int interval = 1;
+        const int maxIterations = 10;
+        for (int time = 0; time < maxIterations; time++) {
+            FieldUtil::display(f.u, time, interval);
+            FieldUtil::display(f.v, time, interval);
+            f = navieStokes.calculate();
+        }
+    } catch (const std::runtime_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
