97ff7c0cc0e238be2158c734530e863b607e1ec0
Kenji-inoue <65485292+Kenji-inoue@users.noreply.github.com>
2025-05-12 07:31:55 +0900

    Used pimpl in burgers

--- 2d/navierStokes/burgers/Burgers2d.cpp
+++ 2d/navierStokes/burgers/Burgers2d.cpp
@@ -1,24 +1,36 @@
+#include "Burgers2d.h"
 #include <string>
 #include <stdexcept>
-#include "Burgers2d.h"
+#include "Advection2d.h"
+#include "Diffusion2d.h"
 #include "FieldUtil.h"
 
+struct Burgers2d::Impl {
+    Diffusion2d diffusion_;
+    Advection2d advection_;
+
+    Impl(int meshX, int meshY, double viscosity, double dx, double dy, double dt)
+        : diffusion_(meshX, meshY, viscosity, dx, dy, dt),
+          advection_(meshX, meshY, dx, dy, dt) {} 
+};
+
 Burgers2d::Burgers2d(int meshX, int meshY, double reynolds, 
-                     double dx, double dy, double deltaT, Velocity2d f) 
+                     double dx, double dy, double dt, Velocity2d f) 
     : MESH_X(meshX), MESH_Y(meshY), REYNOLDS(reynolds), 
-      DX(dx), DY(dy), DELTA_T(deltaT), m_f(f),
-      diffusion_(meshX, meshY, 1/reynolds, DX, DY, deltaT), 
-      advection_(meshX, meshY, DX, DY, deltaT)
+      DX(dx), DY(dy), DT(dt), m_f(f),
+      pimpl_(std::make_unique<Impl>(meshX, meshY, 1/reynolds, dx, dy, dt))
 {
     validateTime();
 }
 
+Burgers2d::~Burgers2d() = default;
+
 void Burgers2d::validateTime() {
-    diffusion_.validateTime();
+    pimpl_->diffusion_.validateTime();
 
     const auto maxU = FieldUtil::findMax(m_f.u);
     const auto maxV = FieldUtil::findMax(m_f.v);
-    advection_.validateTime(maxU, maxV);
+    pimpl_->advection_.validateTime(maxU, maxV);
 }
 
 Velocity2d Burgers2d::calculate() {
@@ -39,11 +51,11 @@ Velocity2d Burgers2d::calculate() {
 }
 
 Value Burgers2d::calculateTerm(const Field2d& f, const Velocity2d& velocity, int i, int j) const {
-    return advection_.calculateVelocity(f, velocity.u, velocity.v, i, j) + diffusion_.calculateTerm(f, i, j);
+    return pimpl_->advection_.calculateVelocity(f, velocity.u, velocity.v, i, j) + pimpl_->diffusion_.calculateTerm(f, i, j);
 }
 
 void Burgers2d::updateBoundaryCondition(Field2d& f) {
-    advection_.updateBoundaryCondition(f);
+    pimpl_->advection_.updateBoundaryCondition(f);
 }
 
 void Burgers2d::updateVelocity(Velocity2d& f) {
--- 2d/navierStokes/burgers/Burgers2d.h
+++ 2d/navierStokes/burgers/Burgers2d.h
@@ -1,14 +1,13 @@
 #pragma once
+#include <memory>
 #include "typedef.h"
-#include "Advection2d.h"
-#include "Diffusion2d.h"
 
 class Burgers2d
 {
 public:
     Burgers2d(int meshX, int meshY, double reynolds, 
               double dx, double dy, double deltaT, Velocity2d f);
-    ~Burgers2d() = default;
+    ~Burgers2d();
 
     Velocity2d calculate();
     Value calculateTerm(const Field2d& f, const Velocity2d& velocity, int i, int j) const;
@@ -16,13 +15,9 @@ public:
     void updateBoundaryCondition(Field2d& f);
 private:
     void updateVelocity(Velocity2d& f);
-    const int MESH_X;
-    const int MESH_Y;
-    const double REYNOLDS;
-    const double DX;
-    const double DY;
-    const double DELTA_T;
+    const int MESH_X, MESH_Y;
+    const double REYNOLDS, DX, DY, DT;
     Velocity2d m_f;
-    Diffusion2d diffusion_;
-    Advection2d advection_;
+    struct Impl;
+    std::unique_ptr<Impl> pimpl_;
 };
